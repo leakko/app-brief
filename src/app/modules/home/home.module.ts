b59a38c17f896202cd9e331d00bc46e7f2ebabf0
Marcos Rodriguez <Marcos.Rodriguez@geograma.com>
2022-06-17 13:15:39 +0200

    Logout implemented

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
+import { SharedModule } from '../shared/shared.module';
 
 
 
@@ -9,7 +10,8 @@ import { HomeComponent } from './home.component';
   declarations: [HomeComponent],
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    SharedModule
   ]
 })
 export class HomeModule { }
